b42709ba6ab6b9bfccb262ee106b30baa14f1f96
VladF <feysaball123@gmail.com>
2023-12-14 23:39:33 +0200

    create all for Product on backend and create Spinner file on front

---
+++ backend/src/store/state.js
@@ -0,0 +1,8 @@
+const state = {
+    user: {
+        token: sessionStorage.getItem('TOKEN'),
+        data: {}
+    }
+}
+
+export default state;
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', [AuthController::class, 'getUser']);
-
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('/product', ProductController::class);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
